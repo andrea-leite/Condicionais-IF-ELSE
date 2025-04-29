a425661423ac1b088f9e2997a30644a2b847d122
Andréa Leite <andrealeite.pf@gmail.com>
2025-04-29 14:32:12 -0300

    Resolução exercício 1

--- src/implementacao/1-soma-maior-que-50.ts
+++ src/implementacao/1-soma-maior-que-50.ts
@@ -1,3 +1,11 @@
-function somaMaiorQueCinquenta() {
-    return null;
+function somaMaiorQueCinquenta(number1:number, number2:number) {
+    let soma = number1 + number2;
+    if (soma > 50){
+        return soma;
+    } else {
+        return null
+    }
+    
 }
+console.log(somaMaiorQueCinquenta(25, 30));
+console.log(somaMaiorQueCinquenta(20, 25));
