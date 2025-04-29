bc6bf44e784ca1894b6f35039081eeeddaad7e49
dronreef2 <202001579176@alunos.estacio.br>
2025-04-29 15:01:22 -0300

    bonificacao

--- src/implementacao/2-bonificacao-vendedor.ts
+++ src/implementacao/2-bonificacao-vendedor.ts
@@ -1,4 +1,16 @@
-function calcularBonificacao() {
-    return null;
-}
-
+function calcularBonificacao(valorUnitario: number, quantidade: number) {
+    let valorVenda: number = valorUnitario * quantidade;
+    
+    if (valorVenda > 100) {
+     let bonificacao: number = 0;
+     if(valorVenda > 100) {
+         bonificacao = valorVenda * 0.10;
+     }
+     return `Valor total: R$${valorVenda}. Bonificação de 10% para o vendedor!Valor da bonificação R$${bonificacao}`;
+    } else {
+     return `Valor total: R$${valorVenda}`;
+    }
+ }
+ 
+ console.log(calcularBonificacao(20, 6));
+ console.log(calcularBonificacao(15, 4));
