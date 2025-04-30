0c124839bb7201be83f39e3651c1f1ec25488b13
dronreef2 <202001579176@alunos.estacio.br>
2025-04-30 14:17:12 -0300

    4 manor maior

--- src/implementacao/4-menor-maior-valor.ts
+++ src/implementacao/4-menor-maior-valor.ts
@@ -1,4 +1,21 @@
-function determinarMenorMaior() {
-    return null;
-}
+function determinarMenorMaior(a: number, b: number): [number, number] {
+    let maior: number;
+    let menor: number;
 
+    if (a > b) {
+        maior = a;
+        menor = b;
+    } else if (b > a) {
+        maior = b;
+        menor = a;
+    } else {
+        maior = a;
+        menor = b;
+    }
+        return [maior, menor];
+    }
+    console.log(determinarMenorMaior(5, 10));   // Saída: [10, 5]
+    console.log(determinarMenorMaior(-3, 2));   // Saída: [2, -3]
+    console.log(determinarMenorMaior(7, 7));    // Saída: [7, 7]
+    console.log(determinarMenorMaior(0, 100));  // Saída: [100, 0]
+    console.log(determinarMenorMaior(-5, -10)); // Saída: [-5, -10
