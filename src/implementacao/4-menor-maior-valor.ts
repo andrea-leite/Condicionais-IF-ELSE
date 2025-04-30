6a76e5f7324af54a5db4a21b120ca2a7460ebf6d
Andréa Leite <andrealeite.pf@gmail.com>
2025-04-30 15:06:51 -0300

    Resolução do exercicio 5.

--- src/implementacao/4-menor-maior-valor.ts
+++ src/implementacao/4-menor-maior-valor.ts
@@ -14,8 +14,8 @@ function determinarMenorMaior(a: number, b: number): [number, number] {
     }
         return [maior, menor];
     }
-    console.log(determinarMenorMaior(5, 10));   // Saída: [10, 5]
-    console.log(determinarMenorMaior(-3, 2));   // Saída: [2, -3]
-    console.log(determinarMenorMaior(7, 7));    // Saída: [7, 7]
-    console.log(determinarMenorMaior(0, 100));  // Saída: [100, 0]
-    console.log(determinarMenorMaior(-5, -10)); // Saída: [-5, -10
+    console.log(determinarMenorMaior(5, 10)); 
+    console.log(determinarMenorMaior(-3, 2)); 
+    console.log(determinarMenorMaior(7, 7)); 
+    console.log(determinarMenorMaior(0, 100));
+    console.log(determinarMenorMaior(-5, -10));
--- src/implementacao/5-par-ou-impar.ts
+++ src/implementacao/5-par-ou-impar.ts
@@ -1,4 +1,15 @@
-function verificarParOuImpar() {
-    return null;
+function verificarParOuImpar() : string {
+    const readlineSync = require('readline-sync');
+    
+    const numero = readlineSync.question('Digite um número: ');
+    const n = parseInt(numero);
+
+    if (n % 2 == 0) {
+        return `${n} é Par`;
+    }else {
+        return `${n} é Ímpar`;        
+    }
 }
+console.log(verificarParOuImpar());
+
 
