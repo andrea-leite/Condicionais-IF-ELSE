cae73811d28ac98004c0369fe8cb17ca4598a3e5
Andréa Leite <andrealeite.pf@gmail.com>
2025-05-02 11:20:00 -0300

    Resolução exercício 6

--- src/implementacao/6-multiplo-de-3.ts
+++ src/implementacao/6-multiplo-de-3.ts
@@ -1,4 +1,14 @@
 function verificarMultiploDe3() {
-    return null;
+    const readlineSync = require('readline-sync');
+    
+    const numero = readlineSync.question('Digite um número: ');
+    const n = parseInt(numero);
+    if (numero % 3 === 0) {
+        return `O número ${numero} é multiplo de 3`;
+    }else {
+        return `O número ${numero} não é multiplo de 3`; 
+    }
 }
+
+console.log(verificarMultiploDe3());
 
