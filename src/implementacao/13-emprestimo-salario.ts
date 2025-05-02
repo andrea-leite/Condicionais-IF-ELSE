1e99904f43537c32ddde2cc3a597f44d77af0373
Andréa Leite <andrealeite.pf@gmail.com>
2025-05-02 11:56:26 -0300

    Resolução exercício 13

--- src/implementacao/13-emprestimo-salario.ts
+++ src/implementacao/13-emprestimo-salario.ts
@@ -1,3 +1,8 @@
-function verificarEmprestimo() {
-    return null;
+function verificarEmprestimo(salarioBruto: number, valorPrestacao: number): boolean {
+  const limitePrestacao: number = salarioBruto * 0.3;
+  return valorPrestacao <= limitePrestacao;
 }
+
+console.log(verificarEmprestimo(3000,900));
+console.log(verificarEmprestimo(4000,1500));
+console.log(verificarEmprestimo(5000,1200));
