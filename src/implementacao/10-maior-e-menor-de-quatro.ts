4537f919c982187d8ec377d04c9d89d472515e2a
Andréa Leite <andrealeite.pf@gmail.com>
2025-05-02 11:45:46 -0300

    Ajuste do exercício 10

--- src/implementacao/10-maior-e-menor-de-quatro.ts
+++ src/implementacao/10-maior-e-menor-de-quatro.ts
@@ -25,16 +25,17 @@ function determinarMaiorMenorDeQuatro(a:number, b:number, c:number, d:number) {
   }
   
   const resultado1 = determinarMaiorMenorDeQuatro(1, 2, 3, 4);
-  console.log(Maior: ${resultado1[0]}, Menor: ${resultado1[1]}); 
+  console.log(`Maior: ${resultado1[0]}, Menor: ${resultado1[1]}`); 
   
   const resultado2 = determinarMaiorMenorDeQuatro(10, 15, 5, 8);
-  console.log(Maior: ${resultado2[0]}, Menor: ${resultado2[1]});
+  console.log(`Maior: ${resultado2[0]}, Menor: ${resultado2[1]}`);
   
   const resultado3 = determinarMaiorMenorDeQuatro(-1, 0, -10, 20);
-  console.log(Maior: ${resultado3[0]}, Menor: ${resultado3[1]});
+  console.log(`Maior: ${resultado3[0]}, Menor: ${resultado3[1]}`);
   
   const resultado4 = determinarMaiorMenorDeQuatro(7, 7, 7, 7);
-  console.log(Maior: ${resultado4[0]}, Menor: ${resultado4[1]});
+  console.log(`Maior: ${resultado4[0]}, Menor: ${resultado4[1]}`);
   
   const resultado5 = determinarMaiorMenorDeQuatro(100, 25, 50, 75);
-  console.log(Maior: ${resultado5[0]}, Menor: ${resultado5[1]});
+  console.log(`Maior: ${resultado5[0]}, Menor: ${resultado5[1]}`);
+  
