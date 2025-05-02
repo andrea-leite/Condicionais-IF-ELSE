c35dc580373330649c1dd44375059ffe2c20d15d
Andréa Leite <andrealeite.pf@gmail.com>
2025-05-02 11:33:40 -0300

    Resolução exercício 8

--- src/implementacao/8-soma-com-ajuste.ts
+++ src/implementacao/8-soma-com-ajuste.ts
@@ -1,4 +1,12 @@
-function somaComAjuste() {
-    return null;
-}
+function somaComAjuste(x:number, y:number): number {
+    let z:number;
+    z = x + y;
 
+    if(z > 20){
+        return (z + 8);
+    }else {
+        return (z - 5); 
+    }    
+}
+console.log(somaComAjuste(15,10));
+console.log(somaComAjuste(12,7));
