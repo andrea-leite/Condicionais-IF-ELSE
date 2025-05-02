8a65f0a55a69e50b02cefe603a276db796f1fa4d
Andréa Leite <andrealeite.pf@gmail.com>
2025-05-02 11:26:14 -0300

    Resolução exercício 7

--- src/implementacao/7-divisivel-por-outro-numero.ts
+++ src/implementacao/7-divisivel-por-outro-numero.ts
@@ -1,4 +1,18 @@
-function verificarDivisivel() {
-    return null;
-}
+function verificarDivisivel(a: number, b:number) : string{
+    
+    if (a % b == 0) {
+        return "A é divisivel por B";
+    }else {
+        return "A não é dividivel por B";
+    }
+}    
+const readlineSync = require('readline-sync');
 
+const numero = readlineSync.question('Digite um número: ');
+const outroNumero = readlineSync.question('Digite mais um número: ');
+
+const num1 = parseInt(numero);
+const num2 = parseInt(outroNumero);
+
+
+console.log(verificarDivisivel(num1, num2));
