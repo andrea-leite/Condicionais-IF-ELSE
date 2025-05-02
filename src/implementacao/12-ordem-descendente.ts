90b38abdad6ab196c7ded4b266007ef2a2949229
dronreef2 <202001579176@alunos.estacio.br>
2025-05-02 11:54:32 -0300

    12 ordem descentente

--- src/implementacao/12-ordem-descendente.ts
+++ src/implementacao/12-ordem-descendente.ts
@@ -1,3 +1,42 @@
-function ordenarDescendente() {
-    return null;
-}
+function ordenarDescendente(a: number, b: number, c: number): string {
+    let primeiro: number;
+    let segundo: number;
+    let terceiro: number;
+  
+    if (a >= b && a >= c) {
+      primeiro = a;
+      if (b >= c) {
+        segundo = b;
+        terceiro = c;
+      } else {
+        segundo = c;
+        terceiro = b;
+      }
+    } else if (b >= a && b >= c) {
+      primeiro = b;
+      if (a >= c) {
+        segundo = a;
+        terceiro = c;
+      } else {
+        segundo = c;
+        terceiro = a;
+      }
+    } else {
+      primeiro = c;
+      if (a >= b) {
+        segundo = a;
+        terceiro = b;
+      } else {
+        segundo = b;
+        terceiro = a;
+      }
+    }
+  
+    return `${primeiro}, ${segundo}, ${terceiro}`;
+  }
+  
+  console.log(ordenarDescendente(3, 1, 2));   
+  console.log(ordenarDescendente(5, 8, 2)); 
+  console.log(ordenarDescendente(-1, 0, -5)); 
+  console.log(ordenarDescendente(7, 7, 7));
+  console.log(ordenarDescendente(50, 25, 75));
