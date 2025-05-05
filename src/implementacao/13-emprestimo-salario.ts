88ac5372aeaf5fccf4cf93804d020a9e63185000
dronreef2 <202001579176@alunos.estacio.br>
2025-05-05 11:22:44 -0300

    troca de exercicio 13 emprestimo

--- src/implementacao/13-emprestimo-salario.ts
+++ src/implementacao/13-emprestimo-salario.ts
@@ -1,16 +1,8 @@
-function verificarEmprestimo( salarioBruto: number, valorPrestacao: number): boolean {
+function verificarEmprestimo(salarioBruto: number, valorPrestacao: number): string {
   const limitePrestacao: number = salarioBruto * 0.3;
-  const aprovado: boolean = valorPrestacao <= limitePrestacao;
-
-  if (aprovado) {
-    console.log("Empréstimo aprovado!");
-  } else {
-    console.log("Empréstimo não aprovado.");
-  }
-
-  return aprovado;
+  return valorPrestacao <= limitePrestacao ? "Empréstimo aprovado!" : "Empréstimo não aprovado.";
 }
 
 console.log(verificarEmprestimo(3000, 900));
 console.log(verificarEmprestimo(4000, 1500));
-console.log(verificarEmprestimo(5000, 1200));
+console.log(verificarEmprestimo(5000, 1200));
